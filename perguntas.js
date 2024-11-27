2a3bb1ad1f718cdda8fc26c15da73a33e774aec7
VitoriaCRosa <0000110890807xsp@al.educacao.sp.gov.br>
2024-11-27 12:30:45 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Quem é Zé Bombom?',
+    'O diretor da escola Moraes Salles'
+)
+
+criaCartao(
+    'Escola',
+    'Com quem eu me sento na sala de aula?',
+    'Claudinha'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o país do futebol?',
+    'O Brasil'
+)
+
+criaCartao(
+    'Aulas',
+    'Qual a melhor aula pra dormir?',
+    'As aulas do Alex'
+)
